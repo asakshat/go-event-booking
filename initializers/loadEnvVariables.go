24c49c689730e8b5f67c75cdd5143a0b72c119bc
asakshat <asakshat453@gmail.com>
2024-06-17 11:48:42 +0200

    added  proc file

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -1,14 +1,10 @@
 package initializers
 
 import (
-	"log"
-
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	godotenv.Load()
+
 }
