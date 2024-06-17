4234ff89bf9aee51778f80109773be60235f74b2
asakshat <asakshat453@gmail.com>
2024-06-17 14:50:00 +0200

    cors

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:8080", "http://127.0.0.1:8080"},
 		AllowMethods:     []string{"GET", "POST", "DELETE", "PUT", "PATCH"},
-		AllowHeaders:     []string{"Origin"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
